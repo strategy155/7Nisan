19588d4dfb89925ed2228bae633ef51f6b0ce48f
gaurwaithmelui <strategy155@gmail.com>
2017-04-25 20:32:11 +0300

    added some pkl

--- vk_api.py
+++ vk_api.py
@@ -1,13 +1,17 @@
 import requests
 import ujson
+import pickle
+import progressbar
 
 OWNER_ID = '-86678270'
 API_URL = 'https://api.vk.com/method/'
 WALL_GET_METHOD = 'wall.get'
 USER_INFO_METHOD = 'users.get'
 CITY_GET_METHOD = 'database.getCitiesById'
+COMMENT_GET_METHOD = 'wall.getComments'
 API_VERSION = '5.63'
 DEFAULT_MESSAGE = 'Not available'
+bar = progressbar.ProgressBar()
 
 
 def get_resp_with_method(api_url, method, params):
@@ -21,18 +25,15 @@ class VkGroup(object):
     posts = list()
 
     def download_posts(self):
-        wall_get_params = {'owner_id': self.id, 'count': '100', 'version': API_VERSION, 'offset': 0}
+        wall_get_params = {'owner_id': self.id, 'count': '100', 'v': API_VERSION, 'offset': 0}
         while True:
             response_dic = get_resp_with_method(API_URL, WALL_GET_METHOD, wall_get_params)
-            # First element is count of posts, so we exclude it
-            try:
-                posts_dic = response_dic['response'][1:]
-            except IndexError:
+            posts_dic = response_dic['response']['items']
+            if len(posts_dic) < 1 :
                 break
-            for elem in posts_dic:
+            for elem in bar(posts_dic):
                 self.posts.append(Post(elem))
             wall_get_params['offset'] += 100
-            print(posts_dic)
 
     def __init__(self, owner_id):
         self.id = owner_id
@@ -48,46 +49,60 @@ class User(object):
         self.get_user_info()
 
     def get_user_info(self):
-        user_params = {'user_ids': self.id, 'fields': 'city,bdate', 'version': API_VERSION}
+        user_params = {'user_ids': self.id, 'fields': 'city,bdate', 'v': API_VERSION}
         vk_structure = get_resp_with_method(API_URL, USER_INFO_METHOD, user_params)
-        print(vk_structure)
         vk_structure = vk_structure['response'][0]
-        date_struct = vk_structure['bdate'].split('.')
-        city_id = vk_structure['city']
-        date_len = len(date_struct)
-        if date_len == 3:
-            self.age = date_struct[2]
-        self.get_city_by_id(city_id)
+        try:
+            date_struct = vk_structure['bdate'].split('.')
+            date_len = len(date_struct)
+            if date_len == 3:
+                self.age = date_struct[2]
 
-    def get_city_by_id(self, city_id):
-        user_params = {'city_ids': city_id, 'version': API_VERSION}
-        vk_structure = get_resp_with_method(API_URL, CITY_GET_METHOD, user_params)
-        vk_structure = vk_structure['response'][0]
-        print(vk_structure)
-        city_str = vk_structure['name']
-        self.city = city_str
-
+        except KeyError:
+            pass
+        except IndexError:
+            pass
+        try:
+            self.city = vk_structure['city']['title']
+        except KeyError:
+            pass
 
 class Comment(object):
 
-    def __init__(self, comment_id, post, text, author):
-        self.id = comment_id
-        self.post = post
-        self.text = text
-        self.author = author
+    def __init__(self, vk_structure):
+        self.text = vk_structure['text']
+        self.comm_id = vk_structure['id']
+        self.owner = User(vk_structure['from_id'])
 
 
 class Post(object):
+    comments = list()
 
     def __init__(self, vk_structure):
-        print(vk_structure)
         self.text = vk_structure['text']
         self.post_id = vk_structure['id']
-        self.owner_id = vk_structure['to_id']
+        self.owner_id = vk_structure['owner_id']
         try:
             self.signer = User(vk_structure['signer_id'])
         except KeyError:
             self.signer = DEFAULT_MESSAGE
+        self.get_comments()
+
+    def get_comments(self):
+        comments_get_params = {'owner_id': self.owner_id, 'post_id': self.post_id, 'count': '100', 'v': API_VERSION, 'offset': 0}
+        while True:
+            response_dic = get_resp_with_method(API_URL, COMMENT_GET_METHOD, comments_get_params)
+            # First element is count of posts, so we exclude it
+            comments_dic = response_dic['response']['items']
+            if len(comments_dic) < 1:
+                break
+            for elem in comments_dic:
+                self.comments.append(Comment(elem))
+            comments_get_params['offset'] += 100
+
+
 
 if __name__ == '__main__':
-    VkGroup(OWNER_ID)
+    cool_shit = VkGroup(OWNER_ID)
+    with open('cool_shit.pkl', 'wb') as f:
+        pickle.dump(cool_shit, f)
